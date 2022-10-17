9b5f4b49a7072303cb1fba13ffd801b656dc92e4
Praveen Yadav <praveen.yadav@agricorlabs.com>
2022-10-17 07:38:32 +0000

    Add project files.

---
+++ WebApplication6/Controllers/CommunicationController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication6.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CommunicationController : ControllerBase
+    {
+        private Communication _option;
+
+        public CommunicationController(IOptionsSnapshot<Communication> options)
+        {
+            _option = options.Value;
+        }
+
+        [HttpGet]
+        public  IActionResult Get(string GridHtml)
+        {
+            var x = _option.CommunicationBaseURL;
+            return Ok();
+          
+        }
+    }
+}
---
+++ WebApplication6/Models/Communication.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication6.Controllers
+{
+    
+    public class CommunicationEndPoints
+    {
+        public string GetTaskDetails { get; set; }
+        public string UpdateTaskDetails { get; set; }       
+    }
+
+    public class Communication
+    {
+        [Required]
+        public string CommunicationBaseURL { get; set; }
+
+        [Range(10,100)]
+        public int Count { get; set; }
+        public CommunicationEndPoints CommunicationEndPoints { get; set; }
+    }
+}
